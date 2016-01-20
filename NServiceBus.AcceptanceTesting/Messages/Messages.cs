93cf188086b7937cd925f84762ca4a1f81706e89
Arnau Julià <arnau.julia@gmail.com>
2016-01-20 13:45:59 +0100

    first commit

---
+++ NServiceBus.AcceptanceTesting/Messages/Messages.cs
@@ -0,0 +1,17 @@
+﻿namespace Messages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NServiceBus;
+
+    public class Messages
+    {
+        public class Command1 : IEvent
+        {
+            
+        }
+    }
+}
